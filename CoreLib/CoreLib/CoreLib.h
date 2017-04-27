2f2aba470e8e4795d545bd884dd451ef23f448ba
Eric Rao <eric.rao@LMXMN090.local>
2017-04-27 21:56:17 +0800

    Add subProject.

---
+++ CoreLib/CoreLib/CoreLib.h
@@ -0,0 +1,13 @@
+//
+//  CoreLib.h
+//  CoreLib
+//
+//  Created by Eric Rao on 27/04/2017.
+//  Copyright © 2017 Eric Rao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CoreLib : NSObject
+
+@end
---
+++ CoreLib/CoreLib/CoreLib.m
@@ -0,0 +1,13 @@
+//
+//  CoreLib.m
+//  CoreLib
+//
+//  Created by Eric Rao on 27/04/2017.
+//  Copyright © 2017 Eric Rao. All rights reserved.
+//
+
+#import "CoreLib.h"
+
+@implementation CoreLib
+
+@end
