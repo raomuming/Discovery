c15819260eb7eeedfb48868fe377ac0fae502753
Eric Rao <eric.rao@LMXMN090.local>
2017-04-27 18:07:51 +0800

    create the initial discovery project.

---
+++ Discovery/Discovery/Discovery/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Discovery
+//
+//  Created by Eric Rao on 27/04/2017.
+//  Copyright © 2017 Eric Rao. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
